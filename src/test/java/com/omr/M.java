f42fb50d5743a3b3b993bc65cfac9a5b6889fca7
anurajsakti <anurajsakti22@gmail.com>
2024-09-13 16:08:47 +0530

    Project1

---
+++ src/test/java/com/omr/M.java
@@ -0,0 +1,14 @@
+package com.omr;
+
+public class M {
+	public void r1() {
+		System.out.println("Raje");
+	}
+	public void r2() {
+		System.out.println("vinoth");
+	}
+	public void r3() {
+		System.out.println("krish");
+	}
+
+}
